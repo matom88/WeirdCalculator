7b96f3f57139e8d069949fedf7b4954eee0c1a31
matom <mauricio.torresmejia@justeattakeaway.com>
2022-10-28 12:48:50 +0200

    Initial Commit

---
+++ WeirdCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeirdCalculator
+//
+//  Created by Mauricio Torres Mejia on 28.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
